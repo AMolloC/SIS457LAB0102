9ad768d41347ed47b5b00153ac606058742d0d85
Abel <molloabel34@gmail.com>
2022-10-02 20:01:11 -0400

    0.1

---
+++ principal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "prueba en visual studio" << endl;
+	cout << "Mi nombre es MOLLO CONDORI ABEL, soy estudiante de ing. de sistemas" << endl;
+	cout << "Nacido en Tupiza, estudie la primaria en un campo de nombre san vicente, y la secundaria en mi pueblo natal" << endl;
+	cout << "Actualmente estudio en Sucre, vivo en alquiler." << endl;
+	cout << "ingrese a la carrera mediante examen de admision" << endl;
+	cout << "Escogi esta carrera por que me sento apto con ganas de aprender" << endl;
+	cout << "Durante el confinamiento estuve en Tupiza, trabaje de albanil, no soy independiente, aun vivo con mis padres" << endl;
+	cout << "soy el ultimo hijo, mi padre era minero, mi madre trabajadora del hogar" << endl;
+	cout << "" << endl;
+
+	system("pause");
+	return 0;
+}
